418f801ec6731f3e92a2de85fc32bb3d348a9d0b
muneeb564 <alimuneeb564@gmail.com>
2025-04-18 22:08:18 +0530

    update constants

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,4 +1,4 @@
-const FRONTEND_URL = "http://localhost:3000";
-// const FRONTEND_URL = ["https://www.probestem.com","https://probestem.com"];
+// const FRONTEND_URL = "http://localhost:3000";
+const FRONTEND_URL = ["https://www.probestem.com","https://probestem.com"];
 
 export { FRONTEND_URL };
