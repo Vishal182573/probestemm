507b633cbb8c8f0146af9cc619b80b09e52f2b93
muneeb564 <alimuneeb564@gmail.com>
2025-04-19 16:39:42 +0530

    next config changes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     images: {
       domains: ['res.cloudinary.com',"media.tenor.com"], // Add Cloudinary domain here
     },
+    productionBrowserSourceMaps: true,
   };
   
   export default nextConfig;
